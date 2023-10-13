62578afbb33e6c93bc1a48abd1318fe415dbead9
misdelivery <misdelivery@icloud.com>
2023-10-14 05:13:53 +0900

    エラー修正

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ if "messages" not in st.session_state.keys():
 def load_data():
     with st.spinner(text="読み込み中・・・"):
         gdd.download_file_from_google_drive(file_id='1aEhDmb0mXCTIWrDSMFxvgpIHQJoyEFGC',
-                                            dest_path='./downloads', 
+                                            dest_path='storage_context.zip', 
                                             unzip=True)
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="ft:gpt-3.5-turbo-0613:personal::87Id1XdJ", temperature=1, max_tokens=220), chunk_size=500)
         storage_context = StorageContext.from_defaults(
-            docstore=SimpleDocumentStore.from_persist_dir(persist_dir="downloads/storage_context"),
-            vector_store=SimpleVectorStore.from_persist_dir(persist_dir="downloads/storage_context"),
-            index_store=SimpleIndexStore.from_persist_dir(persist_dir="downloads/storage_context"),
+            docstore=SimpleDocumentStore.from_persist_dir(persist_dir="storage_context"),
+            vector_store=SimpleVectorStore.from_persist_dir(persist_dir="storage_context"),
+            index_store=SimpleIndexStore.from_persist_dir(persist_dir="storage_context"),
         )
         index = load_index_from_storage(storage_context, service_context=service_context)
         return index
