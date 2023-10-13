123ec2a37ea021c1e4ce83c1937ff3fff40840dc
misdelivery <misdelivery@icloud.com>
2023-10-14 06:12:39 +0900

    b

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def load_data():
         st.write(files)
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="ft:gpt-3.5-turbo-0613:personal::87Id1XdJ", temperature=1, max_tokens=220), chunk_size=500)
         storage_context = StorageContext.from_defaults(
-            docstore=SimpleDocumentStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), 'downloads', 'storage_context'),
+            docstore=SimpleDocumentStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), 'downloads', 'storage_context')),
             vector_store=SimpleVectorStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), 'downloads', 'storage_context')),
             index_store=SimpleIndexStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), 'downloads', 'storage_context')),
         )
