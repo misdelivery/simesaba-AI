c83a4387337a69d9d67370867f8d174fa6af839c
misdelivery <misdelivery@icloud.com>
2023-10-14 06:29:43 +0900

    b

--- main.py
+++ main.py
@@ -30,17 +30,14 @@ if "messages" not in st.session_state.keys():
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="読み込み中・・・"):
-        dest_path=os.path.join(os.getcwd(), 'downloads')
         gdd.download_file_from_google_drive(file_id='1aEhDmb0mXCTIWrDSMFxvgpIHQJoyEFGC',
-                                            dest_path=dest_path,
+                                            dest_path='storage_context.zip',
                                             unzip=True)
-        files = os.listdir(dest_path)
-        st.write(files)
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="ft:gpt-3.5-turbo-0613:personal::87Id1XdJ", temperature=1, max_tokens=220), chunk_size=500)
         storage_context = StorageContext.from_defaults(
-            docstore=SimpleDocumentStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), dest_path, 'storage_context')),
-            vector_store=SimpleVectorStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), dest_path, 'storage_context')),
-            index_store=SimpleIndexStore.from_persist_dir(persist_dir= os.path.join(os.getcwd(), dest_path, 'storage_context')),
+            docstore=SimpleDocumentStore.from_persist_dir(persist_dir= '/storage_context'),
+            vector_store=SimpleVectorStore.from_persist_dir(persist_dir= '/storage_context'),
+            index_store=SimpleIndexStore.from_persist_dir(persist_dir= '/storage_context'),
         )
         index = load_index_from_storage(storage_context, service_context=service_context)
         return index
