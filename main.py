4f22557c655bd9221c176c3ae3a974a086624168
misdelivery <misdelivery@icloud.com>
2023-10-22 03:58:30 +0900

    がんばれ！

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ from generate_audio import inference
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-directory_contents = os.listdir()
-st.write(directory_contents)
+os.chdir('monotonic_align/monotonic_align')
+os/system('python setup.py build_ext --inplace')
 
 import logging
 import sys
