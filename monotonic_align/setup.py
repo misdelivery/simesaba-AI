515f58f45ee213adc18f116639f2183439cb8d06
misdelivery <misdelivery@icloud.com>
2023-10-24 18:20:57 +0900

    b

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -1,14 +1,4 @@
 from distutils.core import setup
-import os
-import sys
-import streamlit as st
-
-def package_install():
-  sys.path.append('/home/appuser/.local/bin')
-  os.system("pip install Cython numpy")
-
-package_install()
-
 from Cython.Build import cythonize
 import numpy
 
