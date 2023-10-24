ba46fa1a9f96dc5bf917922e35acc0043851596e
misdelivery <misdelivery@icloud.com>
2023-10-24 21:27:16 +0900

    simesaba_voiceを作成する予定

---
+++ simesaba_voice.py
@@ -0,0 +1,24 @@
+import requests
+import base64
+import numpy as np
+
+def simesaba_voice(text):
+    url = "http://<YOUR_FLASK_APP_IP>:8080/tts"
+    headers = {
+        'Content-Type': 'text/plain'
+    }
+
+    response = requests.post(url, data=text, headers=headers)
+    data = response.json()
+
+    audio = base64.b64decode(data['audio_base64'])
+
+    audio = np.load(audio)
+
+    return audio
+
+def main():
+    None
+
+if __name__ =="__main__":
+    main()
